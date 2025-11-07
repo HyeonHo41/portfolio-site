3256447c8bec0843918789cb55e6047b2c73a75a
HyeonHo41 <kimhyenho41@gmail.com>
2025-11-07 11:36:18 +0900

    2025.11.07 11:36

--- js/main.js
+++ js/main.js
@@ -14,28 +14,28 @@ spyEls.forEach(function (spyEls) {
     .addTo(controller); //컨트롤러에 장면을 할당(필수)
 });
 
-// Swiper 사용
-const swiper = new Swiper(".project .swiper", {
-  // 슬라이드 옵션 지정
-  // Optional parameters
-  direction: "horizontal", //수평 슬라이드(기본값)
-  loop: true,
-  // autoplay:{
-  //   delay:5000     //자동재생 여부 기본값 3초
-  // },
+// // Swiper 사용
+// const swiper = new Swiper(".project .swiper", {
+//   // 슬라이드 옵션 지정
+//   // Optional parameters
+//   direction: "horizontal", //수평 슬라이드(기본값)
+//   loop: true,
+//   // autoplay:{
+//   //   delay:5000     //자동재생 여부 기본값 3초
+//   // },
 
-  // 페이지네이션 옵션
-  pagination: {
-    el: ".project .swiper-pagination",
-    clickable: true, //페이지네이션 오소 제어 가능 여부 true 변경 기본값 false
-  },
+//   // 페이지네이션 옵션
+//   pagination: {
+//     el: ".project .swiper-pagination",
+//     clickable: true, //페이지네이션 오소 제어 가능 여부 true 변경 기본값 false
+//   },
 
-  // 이전/다음 슬라이드 버튼 옵션
-  navigation: {
-    nextEl: ".project .swiper-button-next",
-    prevEl: ".project .swiper-button-prev",
-  },
-});
+//   // 이전/다음 슬라이드 버튼 옵션
+//   navigation: {
+//     nextEl: ".project .swiper-button-next",
+//     prevEl: ".project .swiper-button-prev",
+//   },
+// });
 
 // 모달창 띄우기
 
